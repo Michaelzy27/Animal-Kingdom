4a18cbe5a27cff4ee91d716902fb345e8608eeb3
Michaelzy27 <umehmichael27@gmail.com>
2024-02-04 11:58:42 +0100

    added characteristics to each subclass

--- animalKingdom.js
+++ animalKingdom.js
@@ -1,23 +1,48 @@
 class AnimalKingdom {
-    constructor(eyes, limbs, size) {
+    constructor(animal, eyes, limbs, size) {
         if(this.constructor === AnimalKingdom)
         throw new Error("Class AnimalKingdom cannot be called or instantiated")
 
         this.eyes = eyes
         this.limbs = limbs
         this.size = size
+        this.animal = animal
     }
 
-    coldBlooded(value) {
-         
+    #animalName() {
+        return this.animal
     }
 
-    warmBlooded() {
-
+    #numberOfEyes() {
+        return this.eyes
     }
 
-    withBackbone() {
+    #numberOfLimbs() {
+        return this.limbs
+    }
 
+    #animalSize() {
+        return this.size
+    }
+
+    bloodValue(value) {
+        if(value === true) {
+            this.blood = "cold blooded"
+        } else {
+            this.blood = "warm blooded"
+        }
+    }
+
+    // warmBlooded() {
+
+    // }
+
+    backbone(value) {
+        if(value === true) {
+            this.bone = "has backbone"
+        } else {
+            this.bone = "has no backbone"
+        }
     }
 
     noBackbone() {
@@ -25,38 +50,113 @@ class AnimalKingdom {
 
     }
 
-    animalDetails() {
-        console.log("This animal has " + this.eyes + " eyes, " + this.limbs + " limbs, and is " + this.size + " in size")
+
+
+    animalDetails(bloodvalue, backbone) {
+        this.bloodValue()
+        this.backbone()
+        console.log(this.#animalName() + " has " + this.#numberOfEyes() + " eyes, " + this.#numberOfLimbs() + " limbs, and is " + this.#animalSize() + " in size")
+        console.log(this.#animalName() + " is " + this.blood)
+        console.log(this.#animalName() + " " + this.bone)
     }
 }
 
 class Arthropoda extends AnimalKingdom {
-    constructor(eyes, limbs, size) {
-        super(eyes, limbs, size)
+    constructor() {
+        super("Athropods", 2, 2, "very small")
 
+    }
+
+    bloodValue() {
+        this.blood = "cold-blooded"
+    }
+
+    backbone() {
+        this.bone = "has no backbone"
     }
 
 }
 
 class Fish extends AnimalKingdom {
+    constructor() {
+        super("Fish", 2, 2, "small")
+    }
+
+    bloodValue() {
+        this.blood = "cold-blooded"
+    }
+
+    backbone() {
+        this.bone = "has backbone"
+    }
 
 }
 
 class Amphibia extends AnimalKingdom {
+    constructor() {
+        super("Amphibians", 2, 4, "small")
+    }
+
+    bloodValue() {
+        this.blood = "cold-blooded"
+    }
+
+    backbone() {
+        this.bone = "has backbone"
+    }
 
 }
 
 class Reptiles extends AnimalKingdom {
+    constructor() {
+        super("Reptiles", 2, 4, "big")
+    }
+
+    bloodvalue() {
+        this.blood = "cold-blooded" 
+    }
+
+    backbone() {
+        this.bone = "has backbone"
+    }
 
 }
 
 class Aves extends AnimalKingdom {
+    constructor() {
+        super("Aves", 2, 2, "medium")
+    }
 
+    bloodValue() {
+        this.blood = "warm-blooded"
+    }
+
+    backbone() {
+        this.bone = "has backbone"
+    }
 }
 
 class Mammals extends AnimalKingdom {
+    constructor() {
+        super("Mammals", 2, 4, "very big")
+    }
+
+    bloodValue() {
+        this.blood = "warm-blooded"
+    }
+
+    backbone() {
+        this.bone = "has backbone"
+    }
 
 }
 
-const animal = new Arthropoda(5, 6, "hfrr")
+const animal = new Fish(5, 6, "hfrr")
+// animal.bloodValue()
+// animal.backbone(true)
 animal.animalDetails()
+
+const mich = new Aves()
+mich.animalDetails()
+
+const 
